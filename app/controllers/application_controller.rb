10922b9522a00461743997113c2b784f1bf6884c
Jim Van Fleet <jim@jimvanfleet.com>
2010-04-08 14:01:53 -0400

    Adding page title

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,12 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   layout "default"
+  
+  before_filter :load_barcamp
+  
+  def load_barcamp
+    @barcamp = Barcamp.active.first
+  end
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
