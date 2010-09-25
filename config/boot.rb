47bbe2672f1ab9f1bfc184582b9a94d7f5984e51
bigfleet <jim@jimvanfleet.com>
2010-09-25 17:48:37 -0400

    Maybe it wasn't needed

--- config/boot.rb
+++ config/boot.rb
@@ -109,13 +109,20 @@ end
 class Rails::Boot
   def run
     load_initializer
-
-    Rails::Initializer.class_eval do
-      def load_gems
-        @bundler_loaded ||= Bundler.require :default, Rails.env
-      end
-    end
-
+    extend_environment
     Rails::Initializer.run(:set_load_path)
   end
+ 
+  def extend_environment
+    Rails::Initializer.class_eval do
+      old_load = instance_method(:load_environment)
+      define_method(:load_environment) do
+        Bundler.require Rails.env, :plugins
+        old_load.bind(self).call
+      end
+    end
+  end
 end
+
+# All that for this:
+Rails.boot!
