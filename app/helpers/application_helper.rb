3c55382fcc13173d30344b50da01bea3a9d4e550
bigfleet <jim@jimvanfleet.com>
2011-02-23 12:02:14 -0500

    rails new -f

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,2 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  
-  def page_title
-    if @barcamp
-      @barcamp.title
-    else
-      "BarCamp Schedule"
-    end
-  end
 end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Barcamp::Application.config.secret_token = '194d314437379926f8ec263859fc280801f59aa43d94f26d2e12887d53b7c0d384505342caf650fc5a4fcc0011eae77dc333f2ae684e8daa6b8bf143684aa397'
