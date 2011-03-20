cd00b52430a845532684f32a9fa08366f3c482e9
bigfleet <jim@jimvanfleet.com>
2011-03-20 13:42:11 -0400

    Features passing, naive Twitter and Facebook integration complete

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -11,7 +11,32 @@ Given /^I am signed up as "([^\"]*)"$/ do |email|
   }
 end
 
-Then /^there should be "([^"]*)" users$/ do |user_count|
+Then /^there should be "([^"]*)" users?$/ do |user_count|
   User.count.should == user_count.to_i
 end
+
+Given /^"([^"]*)" has registered with Twitter$/ do |email|
+  When 'I am on the home page'
+  And  'I follow "Sign in with Twitter"'
+  When 'I fill in "Email" with "'+email+'"'
+  And  'I press "Sign up"'
+  And  'I follow "Logout"'
+end
+
+Given /^I register with email "([^"]*)" and password "([^"]*)"$/ do |email, password|
+  When 'I am on the home page'
+  And  'I follow "Sign up"'
+  And  'I fill in "Email" with "'+email+'"'
+  And  'I fill in "Password" with "'+password+'"'
+  And  'I fill in "Password confirmation" with "'+password+'"'
+  And  'I press "Sign up"'
+  And  'I follow "Logout"'
+end
+
+Given /^I register with Facebook$/ do
+  When 'I am on the home page'
+  And  'I follow "Sign in with Facebook"'
+  And  'I follow "Logout"'
+end
+
 
